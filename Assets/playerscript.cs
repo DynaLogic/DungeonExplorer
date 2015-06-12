5df280dedc2c9a49ce0c1aad516e36a33835e118
Johnsmith0508 <loganwaldman@gmail.com>
2015-06-12 17:13:33 -0400

    Initial commit i don't even know anymore

---
+++ Assets/playerscript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		float horTranslate = Input.GetAxis ("Horizontal");
+		float translation = Input.GetAxis ("Vertical");
+		float rotate = Input.GetAxis ("Mouse X");
+		float rotatevert = Input.GetAxis ("Mouse Y");
+		transform.Rotate(new Vector3(rotatevert,rotate,0),Space.World);
+		transform.Translate (horTranslate, 0, translation);
+
+
+	}
+}
